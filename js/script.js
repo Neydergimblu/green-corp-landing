05596b4a5dd6ac28e0b8a95e5cf6821a124c73bf
Антонов Алексей <AntonovAM@p4udp.local>
2021-11-16 10:41:46 +0300

    Добавили шапку и первую секцию

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Слайдер
+// const swiper = new Swiper('.swiper-container', {
+// 	pagination: {
+// 	  el: '.swiper-pagination',
+// 	  tupe: 'progressbar',
+// 	  clickable: true,
+// 	},
+// 	navigation: {
+// 	  nextEl: '.swiper-button-next',
+// 	  prevEl: '.swiper-button-prev',
+// 	},
+//  });
